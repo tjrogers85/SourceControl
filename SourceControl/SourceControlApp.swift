5318785e681f7054b97d788947fc50559c6206a3
tjrogers85 <tjrogers85@gmail.com>
2021-01-04 16:57:21 -0500

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by TJ on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
